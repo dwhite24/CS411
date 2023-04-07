305da3cbcf50a5303dadc484b7b940260ff61347
dwhite <dwhite@wecohospitality.com>
2023-04-07 15:05:19 -0400

    database set up

--- Protoype/cs411/urls.py
+++ Protoype/cs411/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('song_by_weather/', include('song_by_weather.urls')),
+    path('', include('song_by_weather.urls')),
     path('admin/', admin.site.urls),
 ]
--- Protoype/song_by_weather/models.py
+++ Protoype/song_by_weather/models.py
@@ -1,25 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    text = models.CharField(max_length=128, null=False)
+    id = models.IntegerField(primary_key=True)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=256, null=False)
+    mid = models.IntegerField(null=False, primary_key=True)
+    genre = models.ManyToManyField(Genre, max_length=1028)
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=256, null=False)
+    id = models.CharField(max_length=16, primary_key=True)
+
+
 class User(models.Model):
     first_name = models.CharField(max_length=64, null=False)
     last_name = models.CharField(max_length=64, null=False)
     email = models.EmailField(max_length=128, null=False, primary_key=True)
     username = models.CharField(max_length=64, null=False)
-    age = models.IntegerField(max_length=8, null=True)
+    age = models.IntegerField(null=True)
     hometown = models.CharField(max_length=64, null=True)
     dob = models.DateField(max_length=32, null=True)
+    recommended_movies = models.ManyToManyField(Movie, max_length=2048)
+    recommended_songs = models.ManyToManyField(Song, max_length=2048)
 
-
-class Movie(models.Model):
-    name = models.CharField(max_length=256, null=False)
-    mid = models.IntegerField(max_length=16, null=False, primary_key=True)
-    MOVIE_GENRE_CHOICES = [
-        ('FR', 'Freshman'),
-        ('SO', 'Sophomore'),
-        ('JR', 'Junior'),
-        ('SR', 'Senior'),
-        ('GR', 'Graduate'),
-    ]
-    genre = models.CharField(models.TextChoices, max_length=50)
 
