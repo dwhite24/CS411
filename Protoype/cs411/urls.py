42f0bb8b79eaccd946e5f65d332cbc513d56bdaf
achilles <gitlablsdjf@proton.me>
2023-04-21 15:26:35 -0400

    adding oauth

--- Protoype/cs411/urls.py
+++ Protoype/cs411/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('song_by_weather.urls')),
+    path('', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
